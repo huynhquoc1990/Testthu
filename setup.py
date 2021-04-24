4ff77bb087de2300863852d679941917556f6e9f
Quoc <huynhquoc1990@gmail.com>
2021-04-24 08:12:16 +0700

    xac nahn

--- setup.py
+++ setup.py
@@ -1,22 +1,12 @@
-from  setuptools import setup
-
-def readme():
-    with open('README.md') as f:
-        return f.read()
+from setuptools import setup
 
 setup(
-    name='Mydemo',
-    description=' Demo Mycode',
-    long_description=readme(),
-    long_description_content_type='text/markdown',
-    classifiers=[],
+    name='Testthu',
+    version='1.0.0',
+    packages=['Testthu'],
     url='https://github.com/huynhquoc1990/Testthu.git',
-    author='huynh tan Quoc',
-    author_email='huynhquoc1990@gmail.com',
-    keywords='',
     license='MIT',
-    packages=[],
-    install_requires=[''],
-    include_package_data=True,
-    zip_safe=False
+    author='Quoc',
+    author_email='huynhquoc1990@gmail.com',
+    description='thu'
 )
